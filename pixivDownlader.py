8f76ec78a554912959554b4bd1c03dcd26b57b54
Ktmind <70998992+CCDAYY@users.noreply.github.com>
2022-11-24 13:47:36 +0800

    Add files via upload

--- pixivDownlader.py
+++ pixivDownlader.py
@@ -7,10 +7,10 @@ from tqdm import  tqdm
 import os
 save_path = ""
 
-c_k = input("type y to change cookies->")
+#c_k = input("type y to change cookies->")
 cook = "first_visit_datetime_pc=2021-09-02+23%3A43%3A13; p_ab_id=3; p_ab_id_2=6; p_ab_d_id=1362319497; yuid_b=N0F1eBc; privacy_policy_notification=0; b_type=1; ki_r=; login_ever=yes; ki_s=214908%3A0.0.0.0.2%3B214994%3A0.0.0.0.2%3B215190%3A0.0.0.0.2%3B219376%3A0.0.0.0.2%3B221691%3A0.0.0.0.2; ki_t=1632313976963%3B1640175147711%3B1640175165221%3B5%3B49; c_type=25; a_type=1; privacy_policy_agreement=5; _gcl_au=1.1.1543749531.1664107948; device_token=a9b76dd3d13a3deb263669b9715aadcf; first_visit_datetime=2022-11-03+18%3A26%3A00; PHPSESSID=52047359_GX3stRHI2353liHSwVNL08254B5edAc3; _ga_MZ1NL4PHH0=GS1.1.1667521370.6.0.1667521370.0.0.0; tag_view_ranking=_EOd7bsGyl~lkoWqucyTw~uusOs0ipBx~ziiAzr_h04~jhuUT0OJva~ZNRc-RnkNl~B6uEbiYg7i~CluIvy4vsU~fHzsW6IqUG~RTJMXD26Ak~mkdwargRR2~_hSAdpN9rx~yTWt5hzG4w~P8OX_Lzc1b~qDi7263PSz~dI30gMiyFa~pzZvureUki~q1r4Vd8vYK~CbkyggmWCV~nWC-P2-9TI~ZPjQtvhTg3~UotTWDag3B~mLrrjwTHBm~9Gbahmahac~6293srEnwa~MnGbHeuS94~RgJEiMBANx~_wgwZ79S9p~bv3Hjql-Z1~Ie2c51_4Sp~9dh32MPwDj~Lt-oEicbBr~JL8rvDh62i~JrQgdjRZtN~Bzyu1zjric~EUwzYuPRbU~QliAD3l3jr~w8ffkPoJ_S~K8esoIs2eW~P-Zsw0n2vU~pzzjRSV6ZO~LJo91uBPz4~q3eUobDMJW~bq1HPY2wZ-~pnCQRVigpy~rOnsP2Q5UN~CHzc3gIECp~hZzvvipTPD~TqiZfKmSCg~faZX-CfhYv~yREQ8PVGHN~1s4b4irzBH~QIc0RHSvtN~hW_oUTwHGx~BSlt10mdnm~OUqETMPW2Z~YbOo-qnBCR~aTW6kYb0Ak~IsJjJpzDo3~83nP16VbYh~Thyk9saBEx~0IB1cxSXTq~2V0-EgyHVg~CLEmkBaAcu~BOHDnbK1si~FuSOTTQp_1~PiKFMvIHS1~xF0JX9eOwX~BC84tpS1K_~6ImQE2rhA3~jH0uD88V6F~ETjPkL0e6r~v-OL0-Ncw6~jk9IzfjZ6n~D6xAR9Wod9~KvAGITxIxH~YvAixcnlGi~t1Am7AQCDs~sAwDH104z0~IBgoeiGDSP~CMvJQbTsDH~LRbdzYYhoA~RDY8AkVSDu~oCqKGRNl20~j2lJ8_51Vq~npWJIbJroU~Sgh7s9dZ-K~_AKBg0O8RH~8NU7YH_PAG~59dAqNEUGJ~e9EFq9kkOU~08iLUivxxM~Q4duCCWLbW~0zADS3mWo2~mz8TBIAkOD~OTwy05NHTP~gGjtVdrrFe~NE-E8kJhx6~ZMIwqQI05A~zeOOAJeQjD; QSI_S_ZN_5hF4My7Ad6VNNAi=v:0:0; _gid=GA1.2.759326914.1668594580; __cf_bm=LnXXQnjlxs_eClL7mldyr7_34_xU8d3qude8sSWNUFw-1668595597-0-AZkBIqAxiw9NlWYOyx2e2GXKPrFR5MJqxdvLUGT3wioSvzBHjDohnSOTs6VHQ2lUQRie5ZFamW8HzQE2JaYavFSZSUoHJtej01qfQEf1keDuzAcHKb5CPG5wswRXbR5dmBFPSuv+ixfg1CZcegEYgWOTfqpJSilTme0rUhyEU/wdy/ntCs5rtnEi9JivztazmA==; _ga_75BBYNYN9J=GS1.1.1668594574.46.1.1668595597.0.0.0; _ga=GA1.2.1138408233.1634023850; _gat_UA-1830249-3=1"
-if c_k =="y":
-    cook = input("new cookiee->")
+#if c_k =="y":
+    #cook = input("new cookiee->")
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
     'referer': 'https://www.pixiv.net/',
@@ -24,6 +24,11 @@ def download(id, count):
     url = 'https://www.pixiv.net/artworks/%d' % id
     res = requests.get(url, headers=headers)
     pic_url = re.findall(r'"original":"(.+?)"', res.text)[0]
+    '''
+    r = open("request.txt","a",encoding='utf-8')
+    r.write(str(res.text))
+    r.close()
+    '''
     pic_name = re.findall(r'"illustTitle":"(.+?)"', res.text)[0]
         # 获取后缀
     extension = re.findall(r'....$', pic_url)[0]
@@ -53,8 +58,6 @@ def download(id, count):
                 bar.update(size)
         pic_url = re.findall(r'"original":"(.+?)"', res.text)[0]
         pic_url = re.sub('.....$', '', pic_url)
-
-
 def trending():
     id = []
     count = []
@@ -65,104 +68,148 @@ def trending():
         count = count + re.findall('"illust_page_count":"(\d+)"', res.text)
     if len(id) == len(count):
         for i in tqdm(range(0, len(id)),desc="downloading",unit="pic",position=0):
-            download(int(id[i]), int(count[i]))
-            time.sleep(0.01)
 
+            try:
+                download(int(id[i]), int(count[i]))
+            except:
+                time.sleep(0.01)
+                download(int(id[i]), int(count[i]))
 def NormalS():
     global save_path
-    id=[]
+
     name= input("what you want to search:\n->")
     page = 1
+    mode = 0
+    start = 0
+    end = 0
+    tpage = 0
+
     url = 'https://www.pixiv.net/ajax/search/artworks/{}?word={}&order=date_d&mode=all&p={}&s_mode=s_tag_full&type=all&lang=zh&format=json'.format(name, name, page)
-    total = getTotal(url)
+    total = getTotalPage(url)
     print(str(total)+" pages found!")
     dettt = int(input("type 1 search specific pages, or type 2 search a range pages, type 3 download all pages:"))
 
     if dettt==1:
         det = int(input("how many pages you want to download"))
-        id= M_Pid_search(det,0,1,name)
+        mode= 1
+        tpage = det
+
     elif dettt==2:
         star = int(input("start from which pages?:"))
-        end = int(input("start from which pages?:"))
-        id = M_Pid_search(star,end,2,name)
+        endd = int(input("start from which pages?:"))
+        mode =2
+        start = star
+        end = endd
     elif dettt==3:
-        for x in range(total):
-            print("getting ids pls wait")
-            k = 'https://www.pixiv.net/ajax/search/artworks/{}?word={}&order=date_d&mode=all&p={}&s_mode=s_tag_full&type=all&lang=zh&format=json'.format(name, name, x+1)
-            res = requests.get(k, headers=headers)
-            print(k)
-            id = id + re.findall(r'"id":"(.+?)"', res.text)
-            time.sleep(random.randrange(2, 5))
-    else:
-        return False
+        mode =3
 
-    if len(id)==0:
-        return False
-    new = []
-    for x in range(len(id)):
-        if len(id[x]) > 9:
-            pass
-        else:
-             new.append(id[x])
-    id = new
     ## create file
     save_path = str("./ ")[:2] + name
     if os.path.exists(save_path) == True:
         pass
     else:
         os.mkdir(save_path)
-
-    for x in tqdm(range(len(id)),desc="Downloading",unit="pic",position=0,leave=False):
-        try:
-            download(int(id[x]),1)
-            time.sleep(random.randrange(0,3))
-        except:
-            print("fail download id :"+id[x])
-            download(int(id[x]), 1)
-
-
-
-
-
-
+    i = 1
+    while i <= total:
+        if mode ==1:
+            if i <= tpage :
+                id =getpageids(name,None,1,i)
+                if len(id) == 0:
+                    i = total+1
+                iddownloader(id)
+            if i > tpage:
+                i = total+1
+        elif mode ==2:
+            # rage
+            if i ==start and i< end:
+                id = getpageids(name,None,1,i)
+                iddownloader(id)
+            else:
+                i = total+1
+        elif mode ==3:
+            print("download")
+            id = getpageids(name, None, 1, i)
+            iddownloader(id)
+        i+=1
 def changer():
     global save_path
-    #total = 0
     sname = input("type the things that you want to seach :\n")
     likes = int(input("choose either of : 50,100,300,500,1000,5000,10000 :\n"))
-    id=[]
-    rtes = []
+
     likes = str(likes)+"users"
     page = 1
-    url = 'https://www.pixiv.net/ajax/search/artworks/{}%20{}%E5%85%A5%E3%82%8A?word={}%20{}%E5%85%A5%E3%82%8A&order=date_d&mode=all&p={}&s_mode=s_tag&type=all&lang=zh'.format(sname,likes,sname,likes,page)
-    total = getTotal(url)
-    for x in range(total):
-        k = 'https://www.pixiv.net/ajax/search/artworks/{}%20{}%E5%85%A5%E3%82%8A?word={}%20{}%E5%85%A5%E3%82%8A&order=date_d&mode=all&p={}&s_mode=s_tag&type=all&lang=zh'.format(
-            sname, likes, sname, likes, x + 1)
-        res = requests.get(k, headers=headers)
-        id = id + re.findall(r'"id":"(.+?)"', res.text)
 
-
-    if len(id) == 0:
-        return False
     n_Num = sname + str(likes)
     save_path = str("./ ")[:2] + n_Num
-
     if os.path.exists(save_path) == True:
         pass
     else:
         os.mkdir(save_path)
 
+    url = 'https://www.pixiv.net/ajax/search/artworks/{}%20{}%E5%85%A5%E3%82%8A?word={}%20{}%E5%85%A5%E3%82%8A&order=date_d&mode=all&p={}&s_mode=s_tag&type=all&lang=zh'.format(sname,likes,sname,likes,page)
+    page = getTotalPage(url)
+    print(str(page)+" pages found!")
+    a = input("press y to start download")
+    id = []
+    if a == "y":
+
+        for i in range(page):
+            print("download page: "+str(i))
+            id = getpageids(sname,likes,1,i)
+            print(id)
+            if len(id) == 0:
+                return False
+            else:
+                pass
+                iddownloader(id)
+    else:
+        pass
+
+
+def iddownloader(id):
+    isDown = []
     for i in range(len(id)):
-        if len(id[i]) > 9:
+        isDown.append(True)
+
+    for x in tqdm(range(len(id)),desc="Downloading",unit="pic",position=0,leave=False):
+        try:
+            randomid = random.randrange(0,len(id))
+            while not isDown[randomid]:
+                randomid = random.randrange(0,len(id))
+            download(int(id[x]),1)
+        except:
+            print("fail download id :"+id[x])
+            time.sleep(10)
+            download(int(id[x]), 1)
+        time.sleep(random.randrange(0, 3))
+def getpageids(sname,likes,total,page):
+    id = []
+    if likes == None:
+        for x in range(total):
+            k = 'https://www.pixiv.net/ajax/search/artworks/{}?word={}&order=date_d&mode=all&p={}&s_mode=s_tag_full&type=all&lang=zh&format=json'.format(
+                sname, sname, page)
+            res = requests.get(k, headers=headers)
+            id = id + re.findall(r'"id":"(.+?)"', res.text)
+            time.sleep(3)
+    else:
+        for x in range(total):
+            k = 'https://www.pixiv.net/ajax/search/artworks/{}%20{}%E5%85%A5%E3%82%8A?word={}%20{}%E5%85%A5%E3%82%8A&order=date_d&mode=all&p={}&s_mode=s_tag&type=all&lang=zh'.format(sname, likes, sname, likes, page)
+            res = requests.get(k, headers=headers)
+            id = id + re.findall(r'"id":"(.+?)"', res.text)
+            time.sleep(3)
+
+    id = cleanArray(id)
+    return id
+def cleanArray(arr): # 9 digets
+    new = []
+    for x in range(len(arr)):
+        if len(arr[x]) > 9:
             pass
         else:
-            rtes.append(id[i])
-    print(len(id),"pictures")
-    for x in tqdm(range(len(rtes)),desc="downloading",unit="pic",position=0):
-        download(int(rtes[x]), 1)
-        time.sleep(random.randrange(0,3))
-def getTotal(url):
+            new.append(arr[x])
+    arr = new
+    return arr
+def getTotalPage(url):
     res= requests.get(url,headers=headers)
     total = re.findall(r'"total":(.+?),', res.text)
     print(total,"images found")
@@ -177,6 +224,7 @@ def M_Pid_search(num,end,mode,name):
     id =[]
     print("getting ids pls wait for a while")
     if mode ==1:
+        # get total id by pages
         for x in range(num):
             k = 'https://www.pixiv.net/ajax/search/artworks/{}?word={}&order=date_d&mode=all&p={}&s_mode=s_tag_full&type=all&lang=zh&format=json'.format(name, name, x+1)
             print(k)
@@ -197,12 +245,8 @@ def cos(n):
     global save_path
     if n ==1:# search with ranking
 
-        b = (changer())
-        if b:
-            print("finished")
-        else:
-            print("no pictures found")
-    if n ==2 : #trending
+        changer()
+    elif n ==2 : #trending
         day = dateutil.utils.today()
         dd = day.strftime('%d') + day.strftime('%b')
         save_path = str("./_trending%s") %(dd)
@@ -212,12 +256,14 @@ def cos(n):
             os.mkdir(save_path)
         trending()
 
-    if n == 3:
+    elif n == 3:
         a=(NormalS())
         if a:
             print("finished")
         else:
             print("no pictures found")
-#download(101821127,3)
-cos(int(input("1:(searching with rank ) 2:(daily trending mode), 3:(normal searching)\n->")))
+#download(33165101,1)
+#cos(int(input("1:(searching with rank ) 2:(daily trending mode), 3:(normal searching)\n->")))
+#NormalS()
+#id = getpageids("scaramouche","1000",1,1)
 
